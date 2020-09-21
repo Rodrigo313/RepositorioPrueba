5e88c8bbf5a47858264ab29b5cc661f4982c5be5
Rodrigo Pascual Montero <rodrigopascual1@hotmail.com>
2020-09-21 19:20:54 +0200

    Entrar

--- ejercicio1.js
+++ ejercicio1.js
@@ -1,1 +1,12 @@
 alert("¡Hola Mundo 2!");
+function mostrarVariables(){
+    alert('Variable1: ' + variable1); 
+    alert('Variable2: ' + null);
+    variable1 = 34;
+    alert('Variable1: ' + variable1); 
+    alert('Variable3: ' + 15.9); 
+    alert('Multiplicacion: ' + variable1*variable3);  
+    varaible3 = "Ahora soy un texto";
+    alert('Variable3: ' + variable3); 
+    alert('Multiplicacion2: ' + variable3*35); 
+}
