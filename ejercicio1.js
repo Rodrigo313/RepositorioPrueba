361385518cc39f63fb5de538f2852cc9ea24efb3
Rodrigo Pascual Montero <rodrigopascual1@hotmail.com>
2020-09-22 10:44:21 +0200

    Entrar

--- ejercicio1.js
+++ ejercicio1.js
@@ -10,3 +10,25 @@ function mostrarVariables(){
     alert('Variable3: ' + variable3); 
     alert('Multiplicacion2: ' + variable3*35); 
 }
+function comprobarOperadores(){
+    var1 = "Hola";
+    var2 = "Adiós";
+    alert(var1==var2);
+    alert(var1!=var2);
+    alert(var1<var2);
+    alert(var1>var2);
+    alert(var1<=var2);
+    alert(var1>=var2);
+    var3 = 13;
+    var4 = "trece";
+    alert(var3==var4);
+    alert(var3===var4);
+    var5 = 15;
+    var6 = 20;
+    var7 = 25;
+    alert(var5<var6<var7);
+    alert(var5<=var6);
+    alert(var6>var7);
+    var7=!true;
+    alert(var7);
+}
